1420f7783291e0e26b290cfab864bb19970c98e7
crespo21 <59964121+crespo21@users.noreply.github.com>
2020-06-12 14:21:24 +0200

    Update Algorithm.java
    documented, and refactored the interface name from Algorithm to "DiceAlgorithm"

--- src/main/java/Algorithm.java
+++ src/main/java/Algorithm.java
@@ -1,5 +1,11 @@
-public interface Algorithm {
+
+//DiceAlgorithm interface instantiates Die class with 6 side and
+// instantiates the same Die class with 10 sides.
+//This interface allows to create as many Die with whatever size wanted.
+public interface DiceAlgorithm {
         Die theDie = new Die(6,1,1,1,1,1,1);
-        Die theDie1 = new Die(10,1,1,1,1,1,1,1,1,1,1);
+        Die theDie10 = new Die(10,1,1,1,1,1,1,1,1,1,1);
     }
+
+
 
